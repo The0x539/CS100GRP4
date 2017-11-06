cb9ddb9af5cd108ba0c0fbe0561afe6944ca7961
The0x539 <the0x539@gmail.com>
2017-11-06 16:46:16 +0000

    Sample page, seemingly

--- server.py
+++ server.py
@@ -27,7 +27,11 @@ def name(name=None):
 @server.route('/sample')
 def sample():
     return render_template('map.html')
-    
+
+@server.route('/foo')
+def foo():
+    return render_template('foo.html')
+
 @server.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
